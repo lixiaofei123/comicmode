e1678c48738539237516e6e82ab2af7400d6d2ee
lixiaofei <326256365@qq.com>
2020-07-03 02:07:28 +0800

    bugfix

--- src/App.vue
+++ src/App.vue
@@ -213,7 +213,7 @@ export default {
 		},
 		async loadRecords() {
 			this.listAllRecords().then(records => {
-				this.records = records.sort((r1, r2) => r1.lastReadTime - r2.lastReadTime)
+				this.records = records.sort((r1, r2) => r2.lastReadTime - r1.lastReadTime)
 			}).catch(() => {
 				console.error('load read record failed')
 			})
